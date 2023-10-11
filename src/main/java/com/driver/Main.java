922ea297fb00b00339e7518b94936ae9ce094f62
bullet3113 <0hitesh.a@gmail.com>
2023-10-11 09:18:07 +0530

    assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,30 @@ package com.driver;
 
 public class Main {
 
+    static class Product {
+        public Product() {
+
+        }
+
+        public int product(int x, int y) {
+            return 0;
+        }
+
+        public int product(int x, int y, int z) {
+            return 0;
+        }
+
+        public double product(double x, double y) {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        Product p = new Product();
+
+        System.out.println(p.product(0, 0));
+        System.out.println(p.product(0, 0, 0));
+        System.out.println(p.product(0, 0));
+    }
 }
